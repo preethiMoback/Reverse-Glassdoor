663c2242001c3a453aac1fff1c2f7fd32c3d397e
MobackSruthiB <54569977+Mobacksruthib@users.noreply.github.com>
2022-06-25 23:50:47 +0530

    candidate review page updated

--- src/app/dashboardpage/candidate-review/candidate-review.component.ts
+++ src/app/dashboardpage/candidate-review/candidate-review.component.ts
@@ -17,49 +17,88 @@ export class CandidateReviewComponent implements OnInit {
   
   constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        lastName: ['', Validators.required],
-        companyName:['', Validators.required],
-        primaryskill:['', Validators.required],
-        phoneNumber: ['', [
-            Validators.required,
-            Validators.minLength(8),
-            Validators.maxLength(12),
-            Validators.pattern('^[0-9]*$')]],
-        companyWebsite:['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
-        message:['', Validators.required],
-        acceptTerms: [false, Validators.requiredTrue],
-        rating: [3],
-        gender: ['male', [Validators.required]],
-        email: ['', [
-            Validators.required,
-            Validators.minLength(5),
-            Validators.maxLength(80),
-            Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
-          ]],
-    });
-    // this.value = this.registerForm.controls.rating.valueChanges;
+//   ngOnInit() {
+//     this.registerForm = this.formBuilder.group({
+//         name: ['', Validators.required],
+//         lastName: ['', Validators.required],
+//         companyName:['', Validators.required],
+//         primaryskill:['', Validators.required],
+//         countrycode:['',[Validators.required,Validators.pattern('/^(\+?\d{1,3}|\d{1,4})$/')]],
+//         phoneNumber: ['', [
+//             Validators.required,
+//             Validators.minLength(8),
+//             Validators.maxLength(12),
+//             Validators.pattern('^[0-9]*$')]],
+//         companyWebsite:['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
+//         message:['', Validators.required],
+//         acceptTerms: [false, Validators.requiredTrue],
+//         rating: [3],
+//         gender: ['male', [Validators.required]],
+//         email: ['', [
+//             Validators.required,
+//             Validators.minLength(5),
+//             Validators.maxLength(80),
+//             Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+//           ]],
+//     });
+
     
-}
+// }
 
+// get f() { return this.registerForm.controls; }
+// get myForm() {
+//   return this.registrationForm.get('gender');
+// }
+// onSubmit() {
+//     this.submitted = true;
+//     if (this.registerForm.invalid) {
+//         return;
+//     }
+//     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+// }
+// onReset() {
+//       this.submitted = false;
+//       this.registerForm.reset();
+//   }
+ngOnInit() {
+  this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      companyName:['', Validators.required],
+      primaryskill:['', Validators.required],
+      countrycode:['',[]],//[Validators.required,Validators.pattern('/^(\+?\d{1,3}|\d{1,4})$/')]],
+      rating: [3],
+      gender: ['male', [Validators.required]],
+      phoneNumber: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(12),
+          Validators.pattern('^[0-9]*$')]],
+      companyWebsite:['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
+      message:['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue],
+      email: ['', [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(80),
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ]],
+  });
+}
 get f() { return this.registerForm.controls; }
-get myForm() {
-  return this.registrationForm.get('gender');
-}
+
 onSubmit() {
-    this.submitted = true;
-    if (this.registerForm.invalid) {
-        return;
-    }
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  this.submitted = true;
+  if (this.registerForm.invalid) {
+      return;
+  }
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
 }
 
 
 
-  onReset() {
-      this.submitted = false;
-      this.registerForm.reset();
-  }
+onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+}
 }
